0fed8fe4ebecb31d67ce000458b8d3861d4aa88c
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2017-03-25 15:25:55 +0100

    add missing lorem function to botstrapped site

--- resources/src/app.ts
+++ resources/src/app.ts
@@ -1,11 +1,20 @@
 export class App {
     message: string;
-    
+
     constructor() {
         this.message = 'It Works ';
     }
 
     exclaim() {
         this.message += '!!';
+    }
+
+    lorem() {
+        this.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras luctus tempor aliquet. Phasellus "
+            + "ut laoreet urna, et porta justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur"
+            + " ridiculus mus. Maecenas enim lorem, ullamcorper sed semper ut, hendrerit id lacus. Pellentesque in nunc"
+            + " turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et vehicula lectus. Sed urna "
+            + "est, posuere quis bibendum non, dictum quis erat. Ut varius odio non nisi condimentum, ut auctor quam "
+            + "condimentum."
     }
 }
