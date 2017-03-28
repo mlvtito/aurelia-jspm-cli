ca01edd03809b9e4f9128411a59741959190a856
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2017-03-28 23:40:16 +0200

    handle rejections from npm api

--- npm/npm-api.js
+++ npm/npm-api.js
@@ -5,10 +5,19 @@ var NpmApi = function () {};
 NpmApi.prototype.install = function (moduleNames) {
     return new Promise(function (resolve, reject) {
         var npm = require("npm");
-        npm.load({loaded: false, "save-dev":true, "progress":true,"loglevel":"silent", "depth":0}, function (err) {
-            npm.commands.install(moduleNames, function (er, data) {
-                resolve("Success");
-            });
+        npm.load({loaded: false, "save-dev": true, "progress": false, "loglevel": "silent", "depth": 0}, function (err) {
+
+            if (err) {
+                reject(err);
+            } else {
+                npm.commands.install(moduleNames, function (er, data) {
+                    if (er) {
+                        reject(er);
+                    } else {
+                        resolve("Success");
+                    }
+                });
+            }
         });
     });
 };
