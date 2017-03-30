bb6a0ee61a00d7400ff18a510b3e1d2452618374
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2017-03-31 00:58:39 +0200

    install command with npm install fo dependencies et trying to find @types

--- commands/install.js
+++ commands/install.js
@@ -14,7 +14,6 @@ module.exports = function (argv) {
         var projectJson = JSON.parse(fs.readFileSync(process.cwd() + '/package.json', 'utf8'));
         for (var key in projectJson.jspm.dependencies) {
             if (!projectJson.jspm.dependencies[key].startsWith("npm:jspm-nodelibs")) {
-//                toolsDependencies.push("@types/" + key);
                 if (projectJson.jspm.dependencies[key].startsWith("npm:")) {
                     toolsDependencies.push(projectJson.jspm.dependencies[key].substring(4));
                 }
@@ -22,15 +21,56 @@ module.exports = function (argv) {
         }
         for (var key in projectJson.jspm.peerDependencies) {
             if (!projectJson.jspm.peerDependencies[key].startsWith("npm:jspm-nodelibs")) {
-//                toolsDependencies.push("@types/" + key);
                 if (projectJson.jspm.peerDependencies[key].startsWith("npm:")) {
                     toolsDependencies.push(projectJson.jspm.peerDependencies[key].substring(4));
                 }
             }
         }
-        console.log(toolsDependencies)
         return npm.install(toolsDependencies);
+    }).then(function () {
+        var promises = [];
+        var projectJson = JSON.parse(fs.readFileSync(process.cwd() + '/package.json', 'utf8'));
+        for (var key in projectJson.jspm.dependencies) {
+            if (!projectJson.jspm.dependencies[key].startsWith("npm:jspm-nodelibs")) {
+                if (projectJson.jspm.dependencies[key].startsWith("npm:")) {
+                    var depJson = JSON.parse(fs.readFileSync(process.cwd() + '/node_modules/' + key + '/package.json', 'utf8'));
+                    if (!depJson.typings) {
+                        promises.push(npm.exists("@types/" + key));
+                    }
+                } else {
+                    promises.push(npm.exists("@types/" + key));
+                }
+            }
+        }
+        for (var key in projectJson.jspm.peerDependencies) {
+            if (!projectJson.jspm.peerDependencies[key].startsWith("npm:jspm-nodelibs")) {
+                if (projectJson.jspm.peerDependencies[key].startsWith("npm:")) {
+                    var depJson = JSON.parse(fs.readFileSync(process.cwd() + '/node_modules/' + key + '/package.json', 'utf8'));
+                    if (!depJson.typings) {
+                        promises.push(npm.exists("@types/" + key));
+                    }
+                } else {
+                    promises.push(npm.exists("@types/" + key));
+                }
+            }
+        }
+        return Promise.all(promises.map(reflect));
+    }).then(function (results) {
+        var success = results.filter(x => x.status === "resolved").map(x => x.v.module);
+        if (success.length > 0) {
+            return npm.install(success);
+        }
     }).catch(function (error) {
         console.log("Failed!", error);
     });
 };
+
+
+function reflect(promise) {
+    return promise.then(function (v) {
+        return {v: v, status: "resolved"};
+    },
+            function (e) {
+                return {e: e, status: "rejected"};
+            });
+}
--- npm/npm-api.js
+++ npm/npm-api.js
@@ -1,12 +1,13 @@
 "use strict";
 
+var npm = require("npm");
+var childProcess = require('child_process');
+
 var NpmApi = function () {};
 
 NpmApi.prototype.install = function (moduleNames) {
     return new Promise(function (resolve, reject) {
-        var npm = require("npm");
         npm.load({loaded: false, "save-dev": true, "progress": true, "loglevel": "silent", "depth": 0}, function (err) {
-
             if (err) {
                 reject(err);
             } else {
@@ -17,6 +18,18 @@ NpmApi.prototype.install = function (moduleNames) {
                         resolve("Success");
                     }
                 });
+            }
+        });
+    });
+};
+
+NpmApi.prototype.exists = function (moduleName) {
+    return new Promise(function (resolve, reject) {
+        childProcess.exec("npm info " + moduleName, function (err, stdout, stderr) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({module: moduleName, info: stdout});
             }
         });
     });
