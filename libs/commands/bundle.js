f4d175a0b0e626db16181462324f8278bdff8dda
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2017-04-14 01:09:11 +0200

    better bundle command for vendor and app

--- libs/commands/bundle.js
+++ libs/commands/bundle.js
@@ -3,10 +3,23 @@
 var project = require("../commons/project").project;
 var Builder = require('jspm').Builder;
 
-module.exports = function (argv) {
-    console.log("Bundling...");
-    var modules = project.vendorDependencies().map(d => d.moduleName).reduce((a,b) => a + " + " + b);
-    console.log(modules);
+module.exports = function (options) {
     var builder = new Builder();
-    builder.bundle(modules, "bundles/bundle-vendor.js", {minify:true, inject:false, sourceMaps:true});
+    var message = "Bundling ";
+    if (options.vendor) {
+        message = message + "Vendor Dependencies";
+        if( options.app ) {
+            message = message + " and ";
+        }
+        var modules = project.vendorDependencies().map(d => d.moduleName).reduce((a, b) => a + " + " + b);
+        builder.bundle(modules, "bundles/bundle-vendor.js", {minify: true, inject: false, sourceMaps: true});
+    }
+    
+    if (options.app) {
+        message = message + "App...";
+        var modules = "src/**/*.ts + src/**/*.html!text - " + project.vendorDependencies().map(d => d.moduleName).reduce((a, b) => a + " - " + b);
+        builder.bundle(modules, "bundles/bundle-app.js", {minify: true, inject: false, sourceMaps: true});
+    }
+    
+    console.log(message);
 };
