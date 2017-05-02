50a2edf03aac723737fa2633c3f737a6bbe1c8d7
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2017-05-02 18:08:26 +0200

    start dev server opening with index-dev.html

--- libs/commands/start.js
+++ libs/commands/start.js
@@ -7,7 +7,11 @@ module.exports = function (argv) {
     bundle.bundle("src/**/*.ts + src/**/*.html!text", "bundles/bundle-app.js",
             {minify: true, inject: false, sourceMaps: true, dev: true, watch: true}).then(function () {
         var liveServer = require("../../node_modules/live-server/index");
-        liveServer.start({watch: [process.cwd() + "/bundles"], file: process.cwd()});
+        liveServer.start({
+            watch: [process.cwd() + "/index-dev.html", process.cwd() + "/bundles"], 
+            file: process.cwd(), 
+            open: '/index-dev.html'
+        });
     }).catch(function (err) {
         console.log("ERR: " + err);
     });
