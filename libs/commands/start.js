edce76f69a416cf86dd49efbaa6238f37832c96d
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2017-05-03 15:47:39 +0200

    use promise when generating index-dev.html

--- libs/commands/start.js
+++ libs/commands/start.js
@@ -4,12 +4,16 @@
 
 module.exports = function (argv) {
     var bundle = require("../../node_modules/jspm/lib/bundle");
-    var bundlePromise = bundle.bundle("src/**/*.ts + src/**/*.html!text", "bundles/bundle-app.js",
-            {minify: true, inject: false, sourceMaps: true, dev: true, watch: true});
 
-
-    writeIndexFileForDev();
-    bundlePromise.then(function () {
+    writeIndexFileForDev().then(function () {
+        return bundle.bundle("src/**/*.ts + src/**/*.html!text", "bundles/bundle-app.js", {
+            minify: true,
+            inject: false,
+            sourceMaps: true,
+            dev: true,
+            watch: true
+        });
+    }).then(function () {
         var liveServer = require("../../node_modules/live-server/index");
         liveServer.start({
             watch: [process.cwd() + "/index-dev.html", process.cwd() + "/bundles"],
@@ -22,16 +26,19 @@ module.exports = function (argv) {
 };
 
 function writeIndexFileForDev() {
-    var fs = require('fs')
-    fs.readFile(process.cwd() + "/index.html", 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        var result = data.replace(/jspm\.browser\.js/g, 'jspm.browser.dev.js');
+    return new Promise(function (resolve, reject) {
+        var fs = require('fs')
+        fs.readFile(process.cwd() + "/index.html", 'utf8', function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            var result = data.replace(/jspm\.browser\.js/g, 'jspm.browser.dev.js');
 
-        fs.writeFile(process.cwd() + "/index-dev.html", result, 'utf8', function (err) {
-            if (err)
-                return console.log(err);
+            fs.writeFile(process.cwd() + "/index-dev.html", result, 'utf8', function (err) {
+                if (err)
+                    reject(err);
+            });
+            resolve("Success");
         });
     });
 }
